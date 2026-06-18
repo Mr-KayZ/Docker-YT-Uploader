db9508138249c0f56e94c72851c7e90a78763338
KayZ <kazimushfik@gmail.com>
2026-06-18 00:21:55 -0600

    Set Astro to SSR mode

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,5 +1,11 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+// Set Astro to SSR mode as we need server-side logic for file watching and API calls
 
-// https://astro.build/config
-export default defineConfig({});
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+
+export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
+});
